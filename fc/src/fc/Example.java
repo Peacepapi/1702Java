90b630c7c8f18f75f15ac9f2c0da8c83ec09bd89
Patrick Walsh <patrick@revature.com>
2017-03-01 13:44:12 -0500

    jsp

---
+++ fc/src/fc/Example.java
@@ -0,0 +1,14 @@
+package fc;
+
+/**
+ * Overly convoluted and incredulously difficult 
+ * business logics
+ *
+ */
+public class Example {
+
+	public void log(String message){
+		System.out.println(message);
+	}
+	
+}
